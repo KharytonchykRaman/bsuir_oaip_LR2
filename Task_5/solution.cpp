c06dc0debd7cd43f2694db4e4a1052cdf93bdb5b
Роман Харитончик <qvelgamer@gmail.com>
2025-09-24 23:54:58 +0300

    task 5 control

--- Task_5/solution.cpp
+++ Task_5/solution.cpp
@@ -6,14 +6,26 @@ int main() {
     cout << "Enter x: ";
     cin >> x;
     cout << "Enter y: ";
-    cin >> y;
+    cin >> x;
 
     // 1st option
     bool condition = x > y;
-    double max1 = condition ? x : y;
+    double max1;
+    if (condition) {
+        max1 = x;
+    }
+    else {
+        max1 = y;
+    }
 
-    //2nd option
-    double max2 = (x > y) ? x : y;
+    // 2nd option
+    double max2;
+    if (x > y) {
+        max2 = x;
+    }
+    else {
+        max2 = y;
+    }
 
     cout << "Max (with bool var): " << max1 << endl;
     cout << "Max (without bool var): " << max2 << endl;
