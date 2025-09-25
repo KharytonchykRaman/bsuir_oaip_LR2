60010ec4fab1e17e3c671c8ca0fe5a55116cbecb
Роман Харитончик <qvelgamer@gmail.com>
2025-09-25 17:47:50 +0300

    task 6 variant 4

--- Task_6/solution.cpp
+++ Task_6/solution.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-	double n, p, x, D, K;
-	cout << "Enter n, p, x, D, K: ";
-	cin >> n >> p >> x >> D >> K;
+	double z, y, x, D, K, C;
+	cout << "Enter z, y, x, D, K, C: ";
+	cin >> z >> y >> x >> D >> K >> C;
 
-	double C = p - n;
-	double B = cos(x);
+	double A = x - y;
+	double B = sqrt(z);
 
-	cout << "Q = " << B * B / K / D + B * pow(C, 3);
+	cout << "T = " << cos(x) + A * A / (K - C * D) - B;
 
 	return 0;
 }
